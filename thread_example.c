01b571633c12dbd67f3d46edf921d279cc0e5584
osc <banorman1@ualr.edu>
2021-04-08 10:57:27 -0600

    Done, transfer back

--- thread_example.c
+++ thread_example.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <pthread.h>
 
+//Int that is output to show that the thread ran
 int my_global_int = 10;
 
+//Declare function that will be threaded. Null pointer return and parameter
 void* threaded_function(void* data);
 
 int main() {
+    //Variable to store ID of thread that will be spawned
     pthread_t thread_id;
     
     printf("The initial value of my_global_int is: %d\n", my_global_int);
+    //Create a thread, giving the ID to thread_id, no attributes, the thread will run
+	//the function threaded_function, and no extra data
     pthread_create(&thread_id, NULL, threaded_function, NULL);
-
+    //Wait for the specified thread to finish
     pthread_join(thread_id, NULL);
 
     printf("The final value of my_global_int is: %d\n", my_global_int);
